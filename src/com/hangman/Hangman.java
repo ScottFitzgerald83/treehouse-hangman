4a5dfdb7cc99d43a0b5393e8b12e80e7766f0164
Scott Fitzgerald <sfitzgerald@eero.com>
2020-07-08 01:18:22 -0500

    initial commit, begin building hangman game

---
+++ src/com/hangman/Hangman.java
@@ -0,0 +1,9 @@
+package com.hangman;
+
+public class Hangman {
+
+  public static void main(String[] args) {
+    // Your incredible code goes here...
+    Game game = new Game("treehouse");
+  }
+}
