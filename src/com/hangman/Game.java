c48cf93f70e1113eac4bdde5d9624a169f2bf496
Scott Fitzgerald <sfitzgerald@eero.com>
2020-07-08 20:19:39 -0500

    fix formatting

--- src/com/hangman/Game.java
+++ src/com/hangman/Game.java
@@ -1,24 +1,24 @@
 package com.hangman;
 
 class Game {
-  private String answer;
-  private String hits;
-  private String misses;
+    private String answer;
+    private String hits;
+    private String misses;
 
-  public Game(String answer) {
-    this.answer = answer;
-    hits = "";
-    misses = "";
-  }
+    public Game(String answer) {
+        this.answer = answer;
+        hits = "";
+        misses = "";
+    }
 
-  public boolean applyGuess(char letter) {
-      boolean isHit = answer.indexOf(letter) != -1;
-      if (isHit) {
-          hits += letter;
-      } else {
-          misses += letter;
-      }
-      return isHit;
-  }
+    public boolean applyGuess(char letter) {
+        boolean isHit = answer.indexOf(letter) != -1;
+        if (isHit) {
+            hits += letter;
+        } else {
+            misses += letter;
+        }
+        return isHit;
+    }
 
 }
