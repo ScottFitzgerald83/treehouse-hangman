bd2109a1acf328d91d5056d2667cfdfe031c1bca
Scott Fitzgerald <sfitzgerald@eero.com>
2020-07-08 20:20:30 -0500

    accept a guess and determine if isHit

--- src/com/hangman/Hangman.java
+++ src/com/hangman/Hangman.java
@@ -2,8 +2,15 @@ package com.hangman;
 
 public class Hangman {
 
-  public static void main(String[] args) {
-    // Your incredible code goes here...
-    Game game = new Game("treehouse");
-  }
+    public static void main(String[] args) {
+        // Your incredible code goes here...
+        Game game = new Game("treehouse");
+        Prompter prompter = new Prompter(game);
+        boolean isHit = prompter.promptForGuess();
+        if (isHit) {
+            System.out.println("We got a hit!");
+        } else {
+            System.out.println("Oops, missed");
+        }
+    }
 }
